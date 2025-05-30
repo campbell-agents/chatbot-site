085c928c8119812fec387db65638a2d899b8c10e
kashi campbell <kashi@kashis-MacBook-Air.local>
2025-05-29 21:37:04 -0700

    Replace <a> tags with <Link> in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
-        <a href="/" style={{ marginRight: '15px', color: '#fff', textDecoration: 'none' }}>Home</a>
-        <a href="/about" style={{ marginRight: '15px', color: '#fff', textDecoration: 'none' }}>About</a>
-        <a href="/reviews" style={{ color: '#fff', textDecoration: 'none' }}>Reviews</a>
+        <Link href="/" style={{ marginRight: '15px', color: '#fff', textDecoration: 'none' }}>Home</Link>
+        <Link href="/about" style={{ marginRight: '15px', color: '#fff', textDecoration: 'none' }}>About</Link>
+        <Link href="/reviews" style={{ color: '#fff', textDecoration: 'none' }}>Reviews</Link>
       </nav>
       <Component {...pageProps} />
     </>
