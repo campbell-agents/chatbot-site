67a6d5a50b89d3be8bfcf52201e16984737df89e
campbell-agents <kashicampbell0@gmail.com>
2025-06-06 17:32:59 -0700

    Add global.d.ts declarations and include in tsconfig

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,10 @@
 // global.d.ts
-import * as React from 'react';
+import * as React from "react";
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      // tell TS about the custom element
-      'n8n-chat-widget': React.DetailedHTMLProps<
+      "n8n-chat-widget": React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement>,
         HTMLElement
       >;
@@ -13,11 +12,8 @@ declare global {
   }
 
   interface Window {
-    // tell TS about the widget initializer
     n8nChatWidget: {
-      init: (opts: { url: string; title?: string }) => void;
+      init(options: { url: string; title?: string }): void;
     };
   }
 }
-
-export {};
